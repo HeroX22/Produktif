f7f04d2e47562c5ffd3481f89d20601b305dbebd
HeroX22 <142104516+HeroX22@users.noreply.github.com>
2024-08-06 12:43:12 +0700

    Update zabbix.sh

--- materi/zabbix.sh
+++ materi/zabbix.sh
@@ -6,6 +6,7 @@ wget https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/zabbix
 dpkg -i zabbix-release_7.0-2+debian12_all.deb
 apt update
 apt install apache2 mariadb-server php libapache2-mod-php
+service mariadb start
 apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
 
 mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;"
@@ -16,3 +17,7 @@ mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "set global log_bin_trust_function_cre
 zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -u zabbix -p zabbix
 
 mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "set global log_bin_trust_function_creators = 0;"
+sed -i "130i DBPassword=" /etc/zabbix/zabbix_server.conf
+
+systemctl restart zabbix-server zabbix-agent apache2
+systemctl enable zabbix-server zabbix-agent apache2
