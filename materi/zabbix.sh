bc9592cbd5c7873fa25e0378194064a6fd67bccf
HeroX22 <142104516+HeroX22@users.noreply.github.com>
2024-08-06 12:56:12 +0700

    Update zabbix.sh

--- materi/zabbix.sh
+++ materi/zabbix.sh
@@ -14,7 +14,7 @@ mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "create user zabbix@localhost identifi
 mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "grant all privileges on zabbix.* to zabbix@localhost;"
 mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "set global log_bin_trust_function_creators = 1;"
 
-zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -u zabbix -p zabbix
+zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -u zabbix -p
 
 mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "set global log_bin_trust_function_creators = 0;"
 sed -i "130i DBPassword=" /etc/zabbix/zabbix_server.conf
@@ -24,8 +24,8 @@ systemctl enable zabbix-server zabbix-agent apache2
 
 clear
 
-echo """installasi zabbix sudah selesai
+echo "installasi zabbix sudah selesai
 username : Admin
 password : zabbix
-user zabbix password :
-"""
+user zabbix password : 
+"
