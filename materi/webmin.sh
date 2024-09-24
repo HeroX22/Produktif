c804669936ee41f9abe7f0a90f51b40af7619342
HeroX22 <142104516+HeroX22@users.noreply.github.com>
2024-09-24 09:19:26 +0700

    Update webmin.sh

--- materi/webmin.sh
+++ materi/webmin.sh
@@ -1,3 +1,3 @@
 curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
 sh setup-repos.sh -y
-apt-get install webmin --install-recommends
+apt-get install webmin --install-recommends -y
