5e6d0dd85cf52310095c1ced07da5b81934d9732
HeroX22 <142104516+HeroX22@users.noreply.github.com>
2024-09-26 09:41:32 +0700

    Update grafana.sh

--- materi/grafana.sh
+++ materi/grafana.sh
@@ -1,5 +1,6 @@
 echo "ketik install untuk menginstal, ketik delete untuk uninstall atau menghapusnya"
 read user_input
+
 if [ "$user_input" = "install" ]; then
     sudo apt-get install -y apt-transport-https software-properties-common wget
     sudo mkdir -p /etc/apt/keyrings/
@@ -7,11 +8,46 @@ if [ "$user_input" = "install" ]; then
     echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
     sudo apt-get update
     sudo apt-get install grafana -y
+
+    echo "configurasi bisa menggunakan systemd/systemctl(1) atau init.d/services(2)"
+    read user_input_config
+    
+    if [ "$user_input_config" = "1" ]; then
+        sudo systemctl daemon-reload
+        sudo systemctl start grafana-server
+        sudo systemctl enable grafana-server.service
+        sudo mkdir -p /etc/systemd/system/grafana-server.service.d/
+        sudo touch /etc/systemd/system/grafana-server.service.d/override.conf
+        
+        sudo tee /etc/systemd/system/grafana-server.service.d/override.conf > /dev/null <<EOL
+[Service]
+# Give the CAP_NET_BIND_SERVICE capability
+CapabilityBoundingSet=CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+
+# A private user cannot have process capabilities on the host's user
+# namespace and thus CAP_NET_BIND_SERVICE has no effect.
+PrivateUsers=false
+EOL
+
+    elif [ "$user_input_config" = "2" ]; then
+        sudo service grafana-server start
+        sudo update-rc.d grafana-server defaults
+        sudo service grafana-server restart
+        # The next line is likely unnecessary since you already started the service.
+        ./bin/grafana server
+
+    else
+        echo "input tidak valid, silahkan konfigurasi sendiri"
+        exit 1
+    fi
+
 elif [ "$user_input" = "delete" ]; then
     sudo systemctl stop grafana-server
     sudo service grafana-server stop
     sudo apt-get remove grafana -y
-    sudo rm -i /etc/apt/sources.list.d/grafana.list  # Ganti -i dengan -f jika ingin tanpa konfirmasi
+    sudo rm -i /etc/apt/sources.list.d/grafana.list
 else
     echo "input tidak valid"
+    exit 1
 fi
