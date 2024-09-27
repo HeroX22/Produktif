42b7c595d88d3afc2b6d00c7c6843b4cf339491a
HeroX22 <142104516+HeroX22@users.noreply.github.com>
2024-09-27 14:51:53 +0700

    Update zabbix.sh

--- materi/zabbix.sh
+++ materi/zabbix.sh
@@ -1,6 +1,5 @@
 # Setting variabel
 MYSQL_USER="root"
-MYSQL_PASSWORD=""
 
 wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu24.04_all.deb
 dpkg -i zabbix-release_6.4-1+ubuntu24.04_all.deb
@@ -9,10 +8,10 @@ apt install apache2 mysql-server php libapache2-mod-php -y
 service mysql start
 apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent -y
 
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;"
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "create user zabbix@localhost identified by 'zabbix';"
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "grant all privileges on zabbix.* to zabbix@localhost;"
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "set global log_bin_trust_function_creators = 1;"
+mysql -u $MYSQL_USER -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;"
+mysql -u $MYSQL_USER -e "create user zabbix@localhost identified by 'zabbix';"
+mysql -u $MYSQL_USER -e "grant all privileges on zabbix.* to zabbix@localhost;"
+mysql -u $MYSQL_USER -e "set global log_bin_trust_function_creators = 1;"
 
 echo "masukan zabbix"
 zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -u zabbix -p zabbix
